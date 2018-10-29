0974ef94207b6d2a2a2f3f7f64c389910a0fca2d
Ron <ronlund91@hotmail.com>
2018-10-29 23:44:48 +0100

    removed title bad for accessbility

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -2,7 +2,7 @@
     <footer class="test">
         <p>Partners</p>
         <a v-for="image in images" :href="image.href">
-          <img :src="(image.src)" :class="imgpartner" :alt="image.alt" :title="image.title"/>
+          <img :src="(image.src)" :class="imgpartner" :alt="image.alt"/>
         </a>
     </footer>
 </template>
@@ -14,8 +14,8 @@ export default{
     data(){
         return{
             images: [
-            {name:"Adobe", href:"https://www.adobe.com", src:require('../assets/img/adobe-logo.png'), alt:"Adobe logo", title:"Adobe" },
-            {name:"Microsoft", href:"https://www.microsoft.com", src:require('../assets/img/MicrosoftPartnerLogo.jpg'), alt:"Microsoft", title:"Microsoft"}]
+            {name:"Adobe", href:"https://www.adobe.com", src:require('../assets/img/adobe-logo.png'), alt:"Adobe logo" },
+            {name:"Microsoft", href:"https://www.microsoft.com", src:require('../assets/img/MicrosoftPartnerLogo.jpg'), alt:"Microsoft"}]
         }
     },
     methods:{
