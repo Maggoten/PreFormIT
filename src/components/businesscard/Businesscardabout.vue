ebd1714388c339513e3a9716af8bb03f6a9c428e
Ron Lund <Ron.lund@preformit.se>
2019-11-29 11:10:58 +0100

    Corrected two misspellings

--- src/components/businesscard/Businesscardabout.vue
+++ src/components/businesscard/Businesscardabout.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="divmaxw">
         <h3 class="text-center">About us</h3>
-        <p class="text-center">We are a company that strives for digitalisation in data capture, which can both come from digital forms as in papper forms. As along as we can help you structure your data, and make it easy for both you and your customers to comsume your services.  
-        We have been helping The Swedish Social Insurance Agency, The Swedish Tax Agency, Swedish Police and SPP with thier forms and webservices for data collection.</p>
+        <p class="text-center">We are a company that strives for digitalisation in data capture, which can both come from digital forms as in paper forms. As along as we can help you structure your data, and make it easy for both you and your customers to comsume your services.  
+        We have been helping The Swedish Social Insurance Agency, The Swedish Tax Agency, Swedish Police and SPP with their forms and webservices for data collection.</p>
     </div>  
 </template>
 
